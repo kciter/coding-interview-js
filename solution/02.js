e44e48ee9431743a5741ac3f320489bdf710b442
Lee Sun-Hyoup <kciter@naver.com>
2024-01-14 00:34:07 +0900

    Change the remove duplicate values code

--- solution/02.js
+++ solution/02.js
@@ -1,5 +1,5 @@
 function solution(arr) {
-  const uniqueArr = Array.from(new Set(arr)); // ➊ 중복값 제거
+  const uniqueArr = [...new Set(arr)]; // ➊ 중복값 제거
   uniqueArr.sort((a, b) => b - a); // ➋ 내림차순 정렬
   return uniqueArr;
 }
@@ -7,4 +7,3 @@ function solution(arr) {
 // TEST 코드 입니다. 주석을 풀고 실행시켜 보세요
 // console.log(solution([4, 2, 2, 1, 3, 4])) // 반환값 : [4, 3, 2, 1]
 // console.log(solution([2, 1, 1, 3, 2, 5, 4])) // 반환값 : [5, 4, 3, 2, 1]
-
