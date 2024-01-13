b387b9d1730bfe6fb96cf9ebc44dc8ff1aba0e6f
Lee Sun-Hyoup <kciter@naver.com>
2024-01-14 00:07:43 +0900

    Add 02.js solution

---
+++ solution/02.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+  const uniqueArr = new Set(arr);
+  uniqueArr.sort((a, b) => a - b);
+  return uniqueArr;
+}
+
+// TEST 코드 입니다. 주석을 풀고 실행시켜 보세요
+// console.log(solution([4, 2, 2, 1, 3, 4])) // 반환값 : [4, 3, 2, 1]
+// console.log(solution([2, 1, 1, 3, 2, 5, 4])) // 반환값 : [5, 4, 3, 2, 1]
+
