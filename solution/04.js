dfb10be2f0f1738f3c0a872587c599e2068fbf13
Lee Sun-Hyoup <kciter@naver.com>
2024-01-14 00:45:08 +0900

    Add 04.js solution

---
+++ solution/04.js
@@ -0,0 +1,35 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42840
+
+function solution(answers) {
+  // ➊ 수포자들의 패턴
+  const patterns = [
+    [1, 2, 3, 4, 5], // 1번 수포자의 찍기 패턴
+    [2, 1, 2, 3, 2, 4, 2, 5], // 2번 수포자의 찍기 패턴
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5], // 3번 수포자의 찍기 패턴
+  ];
+
+  // ➋ 수포자들의 점수를 저장할 배열
+  const scores = [0, 0, 0];
+
+  // ➌ 각 수포자의 패턴과 정답이 얼마나 일치하는지 확인
+  for (let i = 0; i < answers.length; i++) {
+    for (let j = 0; j < patterns.length; j++) {
+      if (answers[i] === patterns[j][i % patterns[j].length]) {
+        scores[j] += 1;
+      }
+    }
+  }
+
+  // ➍ 가장 높은 점수 저장
+  const maxScore = Math.max(...scores);
+
+  // ➎ 가장 높은 점수를 받은 수포자들의 번호를 찾아서 배열에 담음
+  const highestScores = [];
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === maxScore) {
+      highestScores.push(i + 1);
+    }
+  }
+
+  return highestScores;
+}
