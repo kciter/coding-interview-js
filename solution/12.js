57278073fcc3cf12eee9dac6683f8faa0f73ef2f
Lee Sun-Hyoup <kciter@naver.com>
2024-01-14 03:42:41 +0900

    Update code follwing origin

--- solution/12.js
+++ solution/12.js
@@ -5,8 +5,8 @@ function solution(prices) {
   const answer = new Array(n).fill(0); // ➊ 가격이 떨어지지 않은 기간을 저장할 배열
 
   // 스택(stack)을 사용해 이전 가격과 현재 가격을 비교
-  const stack = []; // ➋ 스택 초기화
-  for (let i = 0; i < n; i++) {
+  const stack = [0]; // ➋ 스택 초기화
+  for (let i = 1; i < n; i++) {
     while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
       // ➌ 가격이 떨어졌으므로 이전 가격의 기간을 계산
       const j = stack.pop();
