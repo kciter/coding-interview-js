ce129a5936fd99a59f9d78fa61733472f141077e
Lee Sun-Hyoup <kciter@naver.com>
2024-03-11 02:14:59 +0900

    Fix logic

--- solution/15.js
+++ solution/15.js
@@ -1,3 +1,22 @@
+class Queue {
+  items = [];
+  front = 0;
+  rear = 0;
+
+  push(item) {
+    this.items.push(item);
+    this.rear++;
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  pop() {
+    return this.items[this.front++];
+  }
+}
+
 function solution(N, K) {
   const queue = new Queue();
 
@@ -9,8 +28,8 @@ function solution(N, K) {
   while (queue.size() > 1) {
     // ❷ deque에 하나의 요소가 남을 때까지
     for (let i = 0; i < K - 1; i++) {
-      queue.push(queue.shift()); // ❸ K번째 요소를 찾기 위해
-                                 // 앞에서부터 제거하고 뒤에 추가
+      queue.push(queue.pop()); // ❸ K번째 요소를 찾기 위해
+                               // 앞에서부터 제거하고 뒤에 추가
     }
     queue.pop(); // ❹ K번째 요소 제거
   }
