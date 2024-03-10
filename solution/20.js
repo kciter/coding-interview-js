5a038a7a2b4b802dd8e43b0f194cb07cc0f17745
Lee Sun-Hyoup <kciter@naver.com>
2024-03-11 02:53:19 +0900

    Change naming

--- solution/20.js
+++ solution/20.js
@@ -1,24 +1,24 @@
 function solution(participant, completion) {
   // ➊ 해시 테이블 생성
-  const dic = {};
+  const obj = {};
 
   // ➋ 참가자들의 이름을 해시 테이블에 추가
   for (const p of participant) {
-    if (dic[p]) {
-      dic[p] += 1;
+    if (obj[p]) {
+      obj[p] += 1;
     } else {
-      dic[p] = 1;
+      obj[p] = 1;
     }
   }
 
   // ➌ 완주한 선수들의 이름을 키로 하는 값을 1씩 감소
   for (const c of completion) {
-    dic[c] -= 1;
+    obj[c] -= 1;
   }
 
   // ➍ 해시 테이블에 남아 있는 선수가 완주하지 못한 선수
-  for (const key in dic) {
-    if (dic[key] > 0) {
+  for (const key in obj) {
+    if (obj[key] > 0) {
       return key;
     }
   }
