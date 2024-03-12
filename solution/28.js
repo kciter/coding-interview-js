d54a10ddfc86abb434314f2c3efe73f53a1675c8
Lee Sun-Hyoup <kciter@naver.com>
2024-03-12 23:32:46 +0900

    Add 28.js solution

---
+++ solution/28.js
@@ -0,0 +1,11 @@
+function solution(n, a, b) {
+  const answer = 0;
+  while (a != b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer += 1;
+  }
+  return answer;
+}
+
+console.log(solution(8, 4, 7)); // 3
