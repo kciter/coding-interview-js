72b28d76d24695f0cfb58fd58898054c95563152
Lee Sun-Hyoup <kciter@naver.com>
2024-03-13 09:59:44 +0900

    Add 29.js solution

--- solution/29.js
+++ solution/29.js
@@ -1,5 +1,5 @@
 function solution(enroll, referral, seller, amount) {
-  // ➊ parent 딕셔너리 key는 enroll의 노드, value는 referral의 노드로 구성됨
+  // ➊ parent 오브젝트 key는 enroll의 노드, value는 referral의 노드로 구성됨
   let parent = {};
   for(let i = 0; i < enroll.length; i++) {
     parent[enroll[i]] = referral[i];
@@ -15,19 +15,19 @@ function solution(enroll, referral, seller, amount) {
   for(let i = 0; i < seller.length; i++) {
     // ➍ 판매자가 판매한 총 금액 계산
     let money = amount[i] * 100;
-    let cur_name = seller[i];
+    let curName = seller[i];
 
     // ➎ 판매자부터 차례대로 상위 노드로 이동하며 이익 분배
-    while(money > 0 && cur_name != "-") {
+    while(money > 0 && curName != "-") {
       // ➏ 현재 판매자가 받을 금액 계산(10%를 제외한 금액)
-      total[cur_name] += money - Math.floor(money / 10);
-      cur_name = parent[cur_name];
+      total[curName] += money - Math.floor(money / 10);
+      curName = parent[curName];
 
       // ➐ 10%를 제외한 금액 계산
       money = Math.floor(money / 10);
     }
   }
 
-  // ➑ enroll 리스트의 모든 노드에 대해 해당하는 이익을 리스트로 반환
+  // ➑ enroll 배열의 모든 노드에 대해 해당하는 이익을 배열로 반환
   return enroll.map(name => total[name]);
 }
