7d61e1fd72afec1b9243b01df1d6ce469c928b86
Lee Sun-Hyoup <kciter@naver.com>
2024-04-08 23:49:23 +0900

    Add 47.js solution

---
+++ solution/47.js
@@ -0,0 +1,25 @@
+function solution(N) {
+  const results = []; // ➊ 조합 결과를 담을 리스트
+
+  function backtrack(sum, selected_nums, start) {
+    if (sum === 10) { // ❷ 합이 10이 되면 결과 리스트에 추가
+      results.push(selected_nums);
+      return;
+    }
+
+    for (let i = start; i <= N; i++) { // ❸ 다음에 선택할 수 있는 숫자들을 하나씩 선택하면서
+      if (sum + i <= 10) { // ❹ 선택한 숫자의 합이 10보다 작거나 같으면
+        backtrack(
+          sum + i, selected_nums.concat(i), i + 1
+        ); // ❺ 백트래킹 함수를 재귀적으로 호출합니다.
+      }
+    }
+  }
+
+  backtrack(0, [], 1); // ❻ 백트래킹 함수 호출
+  return results; // ❼ 조합 결과 반환
+}
+
+console.log(solution(5)) // [[1, 2, 3, 4], [1, 4, 5], [2, 3, 5]]
+console.log(solution(2)) // []
+console.log(solution(7)) // [[1, 2, 3, 4], [1, 2, 7], [1, 3, 6], [1, 4, 5], [2, 3, 5], [3, 7], [4, 6]]
