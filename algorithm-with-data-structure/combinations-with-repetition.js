b96e8ce539800068299d88c88a19b75e8befc075
Lee Sun-Hyoup <kciter@naver.com>
2024-04-10 14:34:32 +0900

    Add combinations with repetition example

--- algorithm-with-data-structure/combinations-with-repetition.js
+++ algorithm-with-data-structure/combinations-with-repetition.js
@@ -1,4 +1,4 @@
-function combinations(arr, n) {
+function combinationsWithRepetition(arr, n) {
   // 1개만 뽑는다면 그대로 조합을 반환하며 탈출 조건으로도 사용됩니다.
   if (n === 1) return arr.map((v) => [v]);
   const result = [];
@@ -7,9 +7,9 @@ function combinations(arr, n) {
   arr.forEach((fixed, idx, arr) => {
     // 현재 index 이후 요소를 추출
     // index번째는 선택된 요소
-    const rest = arr.slice(idx + 1);
+    const rest = arr.slice(idx);
     // 선택된 요소 이전 요소들을 제외하고 재귀 호출
-    const combis = combinations(rest, n - 1);
+    const combis = combinationsWithRepetition(rest, n - 1);
     // 선택된 요소와 재귀 호출을 통해 구한 조합을 합침
     const combine = combis.map((v) => [fixed, ...v]);
     // 결과 값을 추가
@@ -20,4 +20,4 @@ function combinations(arr, n) {
   return result;
 }
 
-console.log(combinations([1, 2, 3, 4], 2)); // [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+console.log(combinationsWithRepetition([1, 2], 2)); // [[1, 1], [1, 2], [2, 2]]
