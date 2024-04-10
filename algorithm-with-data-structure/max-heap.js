fa1c89e3fed58413c8d03989cc39f1cc8aa05f85
Lee Sun-Hyoup <kciter@naver.com>
2024-04-10 19:14:12 +0900

    Add max heap

---
+++ algorithm-with-data-structure/max-heap.js
@@ -0,0 +1,79 @@
+class MaxHeap {
+  constructor() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  push(item) {
+    this.items.push(item);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.size() === 0) {
+      return null;
+    }
+
+    const min = this.items[0];
+    this.items[0] = this.items[this.size() - 1];
+    this.items.pop();
+    this.bubbleDown();
+    return min;
+  }
+
+  swap(a, b) {
+    [this.items[a], this.items[b]] = [this.items[b], this.items[a]];
+  }
+
+  bubbleUp() {
+    let index = this.size() - 1;
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.items[parentIndex] >= this.items[index]) {
+        break;
+      }
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+    while (index * 2 + 1 < this.size()) {
+      let leftChild = index * 2 + 1;
+      let rightChild = index * 2 + 2;
+      let smallerChild =
+        rightChild < this.size() &&
+        this.items[rightChild] > this.items[leftChild]
+          ? rightChild
+          : leftChild;
+
+      if (this.items[index] >= this.items[smallerChild]) {
+        break;
+      }
+
+      this.swap(index, smallerChild);
+      index = smallerChild;
+    }
+  }
+}
+
+const heap = new MaxHeap();
+heap.push(5);
+heap.push(3);
+heap.push(10);
+heap.push(1);
+
+console.log(heap.pop()); // 10
+console.log(heap.pop()); // 5
+console.log(heap.pop()); // 3
+
+heap.push(2);
+heap.push(4);
+
+console.log(heap.pop()); // 4
+console.log(heap.pop()); // 2
+console.log(heap.pop()); // 1
