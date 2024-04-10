c4eedc73070aab85d57575f33d6f51a4dc1eb7c8
Lee Sun-Hyoup <kciter@naver.com>
2024-04-11 00:06:48 +0900

    Add 74.js solution

--- solution/73.js
+++ solution/73.js
@@ -5,3 +5,11 @@ function solution(n) {
   }
   return fib[n];
 }
+
+// function solution(n) {
+//   const fib = [0, 1]; // F(0) = 0, F(1) = 1
+//   for (let i = 2; i <= n; i++) {
+//     fib.push((fib[i - 1] + fib[i - 2]));
+//   }
+//   return fib[n] % 1234567;
+// }
