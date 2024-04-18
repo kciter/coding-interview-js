09a2811efb16bb645e32503c337d4fbbf4bdcd9c
Sunhyoup Lee <kciter@naver.com>
2024-04-18 10:16:20 +0900

    Update 01.js

--- solution/01.js
+++ solution/01.js
@@ -3,8 +3,6 @@ function solution(arr) {
   return arr;
 }
 
-// TEST 코드 입니다. 주석을 풀고 실행시켜 보세요.
-// console.log(solution([1, -5, 2, 4, 3])); // 반환값 : [-5, 1, 2, 3, 4]
-// console.log(solution([2, 1, 1, 3, 2, 5, 4])); // 반환값 : [1, 1, 2, 2, 3, 4, 5]
-// console.log(solution([1, 6, 7])); // 반환값 : [1, 6, 7]
-
+console.log(solution([1, -5, 2, 4, 3])); // [-5, 1, 2, 3, 4]
+console.log(solution([2, 1, 1, 3, 2, 5, 4])); // [1, 1, 2, 2, 3, 4, 5]
+console.log(solution([1, 6, 7])); // [1, 6, 7]
